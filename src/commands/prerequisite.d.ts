16346630eaee397d3617e16cb2eabbb4b740a167
scottrippey <scott.william.rippey@gmail.com>
2023-02-26 07:48:09 -0700

    feature(prerequisite): implemented prerequisite methods

--- src/commands/prerequisite.d.ts
+++ src/commands/prerequisite.d.ts
@@ -1,4 +1,18 @@
-export {}
+export {};
 declare global {
-  ///
+  export interface Chainable<Subject = any> {
+    /**
+     * Runs some commands as a prerequisite for the test.
+     * If any of these commands fail, the test will be
+     * reported as "skipped" instead of "failed".
+     */
+    prerequisite(commands: () => void): Chainable<void>;
+
+    /**
+     * Runs some commands as a prerequisite for the test.
+     * If any of these commands fail, the entire test suite will be
+     * reported as "skipped" instead of "failed".
+     */
+    prerequisiteForSuite(commands: () => void): Chainable<void>;
+  }
 }
--- src/commands/prerequisite.js
+++ src/commands/prerequisite.js
@@ -8,3 +8,37 @@ Cypress.Commands.add("prerequisiteForSuite", (commands) => {
   commands();
   cy.then(() => cy.off("fail", skipSuite));
 });
+
+// Check each test to see if the parent was skipped:
+beforeEach(skipCurrentIfSuiteFailed);
+
+/** @returns {Mocha.Context} */
+function getMochaContext() {
+  return cy.state("runnable").ctx;
+}
+function skipCurrent(err) {
+  const ctx = getMochaContext();
+  ctx.skip(); // (throws)
+}
+function skipSuite(err) {
+  const ctx = getMochaContext();
+  const test = ctx.currentTest || ctx.test;
+
+  // Mark the parent as failed
+  if (test.parent) test.parent.prerequisiteForSuiteFailed = true;
+
+  ctx.skip(); // (throws)
+}
+function skipCurrentIfSuiteFailed() {
+  const ctx = getMochaContext();
+  const test = ctx.currentTest || ctx.test;
+
+  // Search ancestors to see if any parent suites were skipped:
+  let parent = test.parent;
+  while (parent) {
+    if (parent.prerequisiteForSuiteFailed === true) {
+      ctx.skip(); // (throws)
+    }
+    parent = parent.parent;
+  }
+}
