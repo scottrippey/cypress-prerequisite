53b895df9816e74b2b0758bb665ef626d877d6df
scottrippey <scott.william.rippey@gmail.com>
2023-02-26 20:25:16 -0700

    feature(prerequisite): fixed types

--- src/commands/prerequisite.d.ts
+++ src/commands/prerequisite.d.ts
@@ -4,20 +4,20 @@ declare global {
     interface ResolvedConfigOptions<ComponentDevServerOpts = any> {
       prerequisiteBehavior?: "skip" | "fail";
     }
-  }
-  export interface Chainable<Subject = any> {
-    /**
-     * Runs some commands as a prerequisite for the test.
-     * If any of these commands fail, the test will be
-     * reported as "skipped" instead of "failed".
-     */
-    prerequisite(commands: () => void): Chainable<void>;
+    export interface Chainable<Subject = any> {
+      /**
+       * Runs some commands as a prerequisite for the test.
+       * If any of these commands fail, the test will be
+       * reported as "skipped" instead of "failed".
+       */
+      prerequisite(commands: () => void): Chainable<void>;
 
-    /**
-     * Runs some commands as a prerequisite for the test.
-     * If any of these commands fail, the entire test suite will be
-     * reported as "skipped" instead of "failed".
-     */
-    prerequisiteForSuite(commands: () => void): Chainable<void>;
+      /**
+       * Runs some commands as a prerequisite for the test.
+       * If any of these commands fail, the entire test suite will be
+       * reported as "skipped" instead of "failed".
+       */
+      prerequisiteForSuite(commands: () => void): Chainable<void>;
+    }
   }
 }
